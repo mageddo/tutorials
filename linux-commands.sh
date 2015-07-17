8d70cb97ab11ab6319e831e8cca290c08c93f349
Elvis de Freitas Souza <edigitalb@gmail.com>
2015-07-17 14:56:58 -0300

    Update linux-commands.sh

--- linux-commands.sh
+++ linux-commands.sh
@@ -1,2 +1,14 @@
 # create user with home
 useradd -m someUser
+
+# turn a user sudo
+sudo usermod -a -G sudo userName
+
+# add a group to a user 
+sudo usermod -a -G groupName userName
+
+# creating a ftp user (this user can not login in the shell) 
+sudo useradd userftp -p your_password -d /var/www -s /bin/false
+
+# chaging a user password
+passwd myUser
